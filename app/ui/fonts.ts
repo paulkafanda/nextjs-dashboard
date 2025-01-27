e2cd3e80967a10f685e77d5fc5e67ed644076947
kafandapaul <paulkaf84@gmail.com>
2025-01-27 14:10:17 +0200

    PPR

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
-import { Inter, Lusitana } from "next/font/google";
+import {Inter, Lusitana} from "next/font/google";
 import {undefined} from "zod";
 
-export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({weight: "400", subsets: ['latin'] });
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({weight: "400", subsets: ['latin']});
